1b7cdc727e73ca60dd2123d4e8326eaaa18b14cc
Cemoo <cemalbayri@gmail.com>
2018-01-05 12:01:37 +0300

    first commit

---
+++ AR_Demo2/MainVC.swift
@@ -0,0 +1,261 @@
+//
+//  MainVC.swift
+//  AR_Demo2
+//
+//  Created by Cemal Bayrı on 19.12.2017.
+//  Copyright © 2017 Cemal Bayrı. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class MainVC: UIViewController, ARSCNViewDelegate,ARSessionDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    var animations = [String: CAAnimation]()
+    var idle:Bool = true
+    var planePosition: SCNVector3! = SCNVector3(x:0,y:0,z:-1)
+    var myPlaneAnchor: ARPlaneAnchor?
+    var isCreated = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene()
+        sceneView.scene = scene
+//        loadAnimations()
+   }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+//        // Create a session configuration
+//        let configuration = ARWorldTrackingConfiguration()
+//
+//        // Run the view's session
+//        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard ARWorldTrackingConfiguration.isSupported else {
+            fatalError("""
+                ARKit is not available on this device. For apps that require ARKit
+                for core functionality, use the `arkit` key in the key in the
+                `UIRequiredDeviceCapabilities` section of the Info.plist to prevent
+                the app from installing. (If the app can't be installed, this error
+                can't be triggered in a production scenario.)
+                In apps where AR is an additive feature, use `isSupported` to
+                determine whether to show UI for launching AR experiences.
+            """) // For details, see https://developer.apple.com/documentation/arkit
+        }
+        
+        /*
+         Start the view's AR session with a configuration that uses the rear camera,
+         device position and orientation tracking, and plane detection.
+         */
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+        
+        // Set a delegate to track the number of plane anchors for providing UI feedback.
+        sceneView.session.delegate = self
+        
+        /*
+         Prevent the screen from being dimmed after a while as users will likely
+         have long periods of interaction without touching the screen or buttons.
+         */
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Show debug UI to view performance metrics (e.g. frames per second).
+        sceneView.showsStatistics = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let location = touches.first!.location(in: sceneView)
+//
+//        // Let's test if a 3D Object was touch
+//        var hitTestOptions = [SCNHitTestOption: Any]()
+//        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+//
+//        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+//
+//        if hitResults.first == nil {
+//            if(idle) {
+//                playAnimation(key: "sword")
+////                playWalkAnimation(key: "walk")
+//            } else {
+//                stopAnimation(key: "sword")
+//            }
+//            idle = !idle
+//            return
+//        }
+//        else {
+//            loadAnimations()
+//        }
+        
+        let touch = touches.first
+        let location = touch?.location(in: sceneView)
+        
+        if(touch?.view == self.sceneView){
+            print("touch working")
+            let viewTouchLocation:CGPoint = touch!.location(in: sceneView)
+            guard let result = sceneView.hitTest(viewTouchLocation, options: nil).first else {
+                return
+            }
+            
+            if result.node.name == "plane" {
+                print("plane e girdi")
+                let hitResults = sceneView.hitTest(location!, types: .existingPlaneUsingExtent)
+                if !isCreated {
+                    if hitResults.count > 0 {
+                        let result: ARHitTestResult = hitResults.first!
+                        let newLocation = SCNVector3Make(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z)
+                        loadAnimations(newLocation)
+                    }
+                }
+               
+            }
+            else {
+                playAnimation(key: "sword")
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    func playAnimation(key: String) {
+        // Add the animation to start playing it right away
+        sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key)
+       
+    }
+    
+    func playWalkAnimation(key: String) {
+        // Add the animation to start playing it right away
+        sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key)
+        
+    }
+    
+    func stopAnimation(key: String) {
+        // Stop the animation with a smooth transition
+        sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
+    }
+    
+    func loadAnimations (_ position: SCNVector3) {
+        // Load the character in the idle animation
+        let idleScene = SCNScene(named: "art.scnassets/GreatSwordIdleFixed.dae")!
+        
+        // This node will be parent of all the animation models
+        let node = SCNNode()
+        
+        // Add all the child nodes to the parent node
+        for child in idleScene.rootNode.childNodes {
+            child.scale = SCNVector3(x:0.01,y:0.01,z:0.01)
+            node.addChildNode(child)
+        }
+        // Set up some properties
+//        node.position = SCNVector3(0, -1, -2)
+    
+        node.position = position
+        node.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        // Add the node to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+        self.isCreated = true
+        // Load all the DAE animations
+        loadAnimation(withKey: "sword", sceneName: "art.scnassets/fixedanimation", animationIdentifier: "fixedanimation-1")
+    }
+    
+    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+        let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae")
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+        
+        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
+            // The animation will only play once
+            animationObject.repeatCount = 1
+            // To create smooth transitions between animations
+            animationObject.fadeInDuration = CGFloat(1)
+            animationObject.fadeOutDuration = CGFloat(0.5)
+            
+            // Store the animation for later use
+            animations[withKey] = animationObject
+        }
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // Update content only for plane anchors and nodes matching the setup created in `renderer(_:didAdd:for:)`.
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // Plane estimation may shift the center of a plane relative to its anchor's transform.
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        
+        /*
+         Plane estimation may extend the size of the plane, or combine previously detected
+         planes into a larger one. In the latter case, `ARSCNView` automatically deletes the
+         corresponding node for one plane, then calls this method to update the size of
+         the remaining plane.
+         */
+        plane.width = CGFloat(planeAnchor.extent.x)
+        plane.height = CGFloat(planeAnchor.extent.z)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // Create a SceneKit plane to visualize the plane anchor using its position and extent.
+        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.simdPosition = float3(planeAnchor.center.x, 0, planeAnchor.center.z)
+        planePosition = planeNode.position
+        self.myPlaneAnchor = planeAnchor
+        /*
+         `SCNPlane` is vertically oriented in its local coordinate space, so
+         rotate the plane to match the horizontal orientation of `ARPlaneAnchor`.
+         */
+        planeNode.eulerAngles.x = -.pi / 2
+        planeNode.name = "plane"
+        // Make the plane visualization semitransparent to clearly show real-world placement.
+        planeNode.opacity = 0.1
+        
+        /*
+         Add the plane visualization to the ARKit-managed node so that it tracks
+         changes in the plane anchor as plane estimation continues.
+         */
+        node.addChildNode(planeNode)
+//        myNodes.append(planeNode)
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
